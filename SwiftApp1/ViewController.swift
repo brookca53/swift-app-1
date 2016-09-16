2b1f0e95496a4dd5b527b4d2d64e2ba251ec886c
Alex <c.alex.brooks@gmail.com>
2016-09-16 00:15:23 -0400

    My first commit

--- SwiftApp1/ViewController.swift
+++ SwiftApp1/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        tapCount += 1
+        
+        
+        if tapCount <= 5 {
+            self.view.backgroundColor = UIColor.red
+            coolLabel.text = "You pushed the button \(tapCount) time(s)!!"
+            print("Normal button tapped: \(tapCount) time(s)!!")
+        } else {
+            self.view.backgroundColor = UIColor.blue
+            coolLabel.text = "Setting button tap count to 0 "
+            tapCount = 0
+        }
+        
+        
+        
+    }
+    
+    
+    @IBAction func coolBtnTap(_ sender: AnyObject) {
+        
+        coolLabel.text = "Buttons are cool!!"
+        print("Cool button tapped")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.green
+        
     }
 
     override func didReceiveMemoryWarning() {
